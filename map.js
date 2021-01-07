eadafcdf0415df0224f4662608e90012f78d660c
Md. Rony <mdronycoder@gmail.com>
2021-01-07 06:03:38 +0600

    done it

---
+++ map.js
@@ -0,0 +1,15 @@
+// const numbers = [4, 6, 8, 10, 12];
+// output = []
+// for (let i = 0; i < numbers.length; i++) {
+//     const element = numbers[i];
+//     const result = element * element;
+//     output.push(result)
+// }
+// console.log(output)
+
+const age = [4, 6, 8, 10, 12];
+
+
+const result = age.map(element => element * element)
+
+console.log(result)
